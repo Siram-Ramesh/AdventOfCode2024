9699af2380774982d4cd0045d8b232797ea94e26
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 15:27:49 -0800

    Update format of testcase files to mirror realData

--- historianHysteria/TestHistorianHysteriaPart1.py
+++ historianHysteria/TestHistorianHysteriaPart1.py
@@ -6,7 +6,7 @@ import HistorianHysteria as hh
 class TestHistorianHisteriaPart1(unittest.TestCase):
 
     def test_LengthOfOne(self):
-        df = pd.read_csv('testcases/lengthOfOne.txt', header=None, delimiter='\t')
+        df = pd.read_csv('testcases/lengthOfOne.txt', header=None, delimiter='   ')
         arr1 = df.iloc[:,0].to_numpy()
         arr2 = df.iloc[:,1].to_numpy()
         
@@ -14,7 +14,7 @@ class TestHistorianHisteriaPart1(unittest.TestCase):
         self.assertEqual(dist, 7)
 
     def test_NoDiff(self):
-        df = pd.read_csv('testcases/noDiff.txt', header=None, delimiter='\t')
+        df = pd.read_csv('testcases/noDiff.txt', header=None, delimiter='   ')
         arr1 = df.iloc[:,0].to_numpy()
         arr2 = df.iloc[:,1].to_numpy()
         
