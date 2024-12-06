0b2eb95cd8e35158bd5d119f46baa8a08f8dbc4e
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 15:11:20 -0800

    Make solution code pass unit tests by resolving syntax issues

--- historianHysteria/HistorianHysteria.py
+++ historianHysteria/HistorianHysteria.py
@@ -1,20 +1,18 @@
 import numpy
 import pandas
 
-class HistorianHysteria:
+def findTotalDistance(arr1, arr2):
+    numpy.sort(arr1)
+    numpy.sort(arr2)
 
-    def findTotalDistance(arr1, arr2):
-        numpy.sort(arr1)
-        numpy.sort(arr2)
-        
-        total = numpy.sum(np.subtract(arr1, arr2))
+    total = numpy.sum(numpy.subtract(arr1, arr2))
 
-        return abs(total)
+    return abs(total)
 
-    if __name__ == '__main__':
-        df = pd.read_csv('realData', header=None, delimiter='\t')
-        self.arr1 = df[[0]].to_numpy()
-        self.arr2 = df[[1]].to_numpy()
-        
-        dist = findTotalDistance(arr1, arr2)
-        print(dist)
+if __name__ == '__main__':
+    df = pandas.read_csv('realData', header=None, delimiter='\t')
+    arr1 = df.iloc[:,0].to_numpy()
+    arr2 = df.iloc[:,1].to_numpy()
+    
+    dist = findTotalDistance(arr1, arr2)
+    print(dist)
