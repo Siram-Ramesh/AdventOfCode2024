b3d2726cfcf931d9e060de0c5ba7c2782dd4dc5a
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 15:31:42 -0800

    Make main function now delimit based on spaces instead of tabs

--- historianHysteria/HistorianHysteria.py
+++ historianHysteria/HistorianHysteria.py
@@ -10,7 +10,7 @@ def findTotalDistance(arr1, arr2):
     return abs(total)
 
 if __name__ == '__main__':
-    df = pandas.read_csv('realData', header=None, delimiter='\t')
+    df = pandas.read_csv('realData', header=None, delimiter='   ')
     arr1 = df.iloc[:,0].to_numpy()
     arr2 = df.iloc[:,1].to_numpy()
     
