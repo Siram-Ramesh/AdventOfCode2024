92b4f688f0deee3cd67e11802a50b4707fa802cd
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 15:10:23 -0800

    Resolve syntax and filestructure issues in Test class

--- historianHysteria/TestHistorianHysteriaPart1.py
+++ historianHysteria/TestHistorianHysteriaPart1.py
@@ -6,20 +6,20 @@ import HistorianHysteria as hh
 class TestHistorianHisteriaPart1(unittest.TestCase):
 
     def test_LengthOfOne(self):
-        df = pd.read_csv('lengthOfOne.txt', header=None, delimiter='\t')
-        self.arr1 = df[[0]].to_numpy()
-        self.arr2 = df[[1]].to_numpy()
+        df = pd.read_csv('testcases/lengthOfOne.txt', header=None, delimiter='\t')
+        arr1 = df.iloc[:,0].to_numpy()
+        arr2 = df.iloc[:,1].to_numpy()
         
         dist = hh.findTotalDistance(arr1, arr2)
-        unittest.assertEqual(dist, 7)
+        self.assertEqual(dist, 7)
 
     def test_NoDiff(self):
-        df = pd.read_csv('noDiff.txt', header=None, delimiter='\t')
-        self.arr1 = df[[0]].to_numpy()
-        self.arr2 = df[[1]].to_numpy()
+        df = pd.read_csv('testcases/noDiff.txt', header=None, delimiter='\t')
+        arr1 = df.iloc[:,0].to_numpy()
+        arr2 = df.iloc[:,1].to_numpy()
         
         dist = hh.findTotalDistance(arr1, arr2)
-        unittest.assertEqual(dist, 0)
+        self.assertEqual(dist, 0)
 
 if __name__ == '__main__':
     unittest.main()
