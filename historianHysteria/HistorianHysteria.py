8665a336ca8ba412d97c360d798c7be38d6a407a
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 19:13:40 -0800

    Pass first 2 test cases of part 2

--- historianHysteria/HistorianHysteria.py
+++ historianHysteria/HistorianHysteria.py
@@ -17,6 +17,35 @@ def findTotalDistance(filename):
 
     return abs(total)
 
+def findSimilarity(filename):
+    df = pandas.read_csv(filename, header=None, delimiter='   ', engine='python')
+    map1 = {}
+    map2 = {}
+
+    for index, row in df.iterrows():
+        if row[0] not in map1:
+            map1[row[0]] = 1
+        else:
+            map1[row[0]] += 1
+        if row[1] not in map2:
+            map2[row[1]] = 1
+        else:
+            map2[row[1]] += 1
+
+    total1 = 0
+    total2 = 0
+    for index, row in df.iterrows():
+        if row[1] in map1:
+            total1 += row[1]*map1[row[1]]
+        if row[0] in map2:
+            total2 += row[0]*map2[row[0]]
+
+    if total1 != total2:
+        raise Exception('The similarity scores of the 2 arrays are different!')
+
+    return total1
+
+
 if __name__ == '__main__':
     
     dist = findTotalDistance('realData')
