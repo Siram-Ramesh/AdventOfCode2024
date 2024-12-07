d43ad2751ff9bdf6816f8c26e2af4102ff24b2c9
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 20:41:06 -0800

    Add test case for a single valid row to TestRedNosedReportsPart1.py

--- redNosedReports/TestRedNosedReportsPart1.py
+++ redNosedReports/TestRedNosedReportsPart1.py
@@ -3,9 +3,14 @@ import RedNosedReports as rnr
 
 class TestRedNosedReportsPart1(unittest.TestCase):
 
+    def test_singleRowAscValid(self):
+        numValid = rnr.countValidReports('testcases/part1/singleRowAscValid.txt')
+        self.assertEqual(numValid, 1)
+
     def test_basic(self):
         numValid = rnr.countValidReports('testcases/part1/basic.txt')
         self.assertEqual(numValid, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
