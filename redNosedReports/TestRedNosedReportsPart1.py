7a5dfd975ae2cba14ef82d302f04ebd112dce0d8
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 20:26:39 -0800

    Fix syntax errors in TestRedNosedReportsPart1.py

--- redNosedReports/TestRedNosedReportsPart1.py
+++ redNosedReports/TestRedNosedReportsPart1.py
@@ -1,10 +1,11 @@
 import unittest
+import RedNosedReports as rnr
 
 class TestRedNosedReportsPart1(unittest.TestCase):
 
-    def test_basic():
+    def test_basic(self):
         numValid = rnr.countValidReports('testcases/part1/basic.txt')
         self.assertEqual(numValid, 2)
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
