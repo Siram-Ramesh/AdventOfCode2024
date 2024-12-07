99c7434b6a7e4eb79f36caedd3a0d1e0373d3867
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 18:36:34 -0800

    create folder specifically for part1 testcase files

--- historianHysteria/TestHistorianHysteriaPart1.py
+++ historianHysteria/TestHistorianHysteriaPart1.py
@@ -6,19 +6,19 @@ import HistorianHysteria as hh
 class TestHistorianHisteriaPart1(unittest.TestCase):
 
     def test_LengthOfOne(self):
-        dist = hh.findTotalDistance('testcases/lengthOfOne.txt')
+        dist = hh.findTotalDistance('testcases/part1/lengthOfOne.txt')
         self.assertEqual(dist, 7)
 
     def test_NoDiff(self):
-        dist = hh.findTotalDistance('testcases/noDiff.txt')
+        dist = hh.findTotalDistance('testcases/part1/noDiff.txt')
         self.assertEqual(dist, 0)
 
     def test_AbsVal(self):
-        dist = hh.findTotalDistance('testcases/absVal.txt')
+        dist = hh.findTotalDistance('testcases/part1/absVal.txt')
         self.assertEqual(dist, 9)
 
     def test_Sorted(self):
-        dist = hh.findTotalDistance('testcases/sorted.txt')
+        dist = hh.findTotalDistance('testcases/part1/sorted.txt')
         self.assertEqual(dist, 5)
 
 if __name__ == '__main__':
