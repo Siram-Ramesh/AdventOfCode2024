09fb56276244fb42c83ea5d07372a4eeb88a4128
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 19:08:28 -0800

    Fix incorrect filenames in Part2 test class

--- historianHysteria/TestHistorianHysteriaPart2.py
+++ historianHysteria/TestHistorianHysteriaPart2.py
@@ -4,15 +4,15 @@ import HistorianHysteria as hh
 class TestHistorianHisteriaPart2(unittest.TestCase):
 
     def test_Basic(self):
-        sims = hh.findSimilarity('testcases/basicPart2.txt')
+        sims = hh.findSimilarity('testcases/part2/basic.txt')
         self.assertEqual(sims, 31)
 
     def test_NoOverlap(self):
-        sims = hh.findSimilarity('testcases/noOverlapPart2.txt')
+        sims = hh.findSimilarity('testcases/part2/noOverlap.txt')
         self.assertEqual(sims, 0)
 
     def test_AbsVal(self):
-        sims = hh.findSimilarity('testcases/absValPart2.txt')
+        sims = hh.findSimilarity('testcases/part2/absVal.txt')
         self.assertEqual(sims, 4)
 
 
