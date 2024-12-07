e66ea763863936acc7471794890af20b0ef28d60
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 19:45:59 -0800

    Setup initial file structure for RedNosedReports

---
+++ redNosedReports/TestRedNosedReportsPart1.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestRedNosedReportsPart1(unittest.TestCase):
+
+    def test_basic():
+        numValid = rnr.countValidReports('testcases/part1/basic.txt')
+        self.assertEqual(numValid, 2)
+
+    if __name__ == '__main__':
+        unittest.main()
