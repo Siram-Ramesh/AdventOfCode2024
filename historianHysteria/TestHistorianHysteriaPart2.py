e476aa3ab634064a526dd0f3ad9d1b753b9a31e0
Sriram Ramesh <srirames403735@gmail.com>
2024-12-06 19:04:18 -0800

    Change return structure of findSimilarity() to a single number because the scores should be identical for both arrays

--- historianHysteria/TestHistorianHysteriaPart2.py
+++ historianHysteria/TestHistorianHysteriaPart2.py
@@ -5,18 +5,15 @@ class TestHistorianHisteriaPart2(unittest.TestCase):
 
     def test_Basic(self):
         sims = hh.findSimilarity('testcases/basicPart2.txt')
-        self.assertEqual(sims[0], 31)
-        self.assertEqual(sims[1], 31)
+        self.assertEqual(sims, 31)
 
     def test_NoOverlap(self):
         sims = hh.findSimilarity('testcases/noOverlapPart2.txt')
-        self.assertEqual(sims[0], 0)
-        self.assertEqual(sims[1], 0)
+        self.assertEqual(sims, 0)
 
     def test_AbsVal(self):
         sims = hh.findSimilarity('testcases/absValPart2.txt')
-        self.assertEqual(sims[0], 4)
-        self.assertEqual(sims[1], 4)
+        self.assertEqual(sims, 4)
 
 
 if __name__ == '__main__':
